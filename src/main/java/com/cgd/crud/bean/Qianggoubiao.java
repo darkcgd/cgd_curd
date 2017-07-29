a8112d75d3f904bf9bd4d897c575dbcc72a82bd1
cgd <492404822@qq.com>
2017-07-29 21:59:43 +0800

    创建了抢购表

---
+++ src/main/java/com/cgd/crud/bean/Qianggoubiao.java
@@ -0,0 +1,85 @@
+package com.cgd.crud.bean;
+
+import java.util.Date;
+
+public class Qianggoubiao {
+    private Integer id;
+
+    private String name;
+
+    private Date createTime;
+
+    private String logo;
+
+    private String remark;
+
+    private String directeUrl;
+
+    private String isInvalid;
+
+    private String createUser;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo == null ? null : logo.trim();
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+
+    public String getDirecteUrl() {
+        return directeUrl;
+    }
+
+    public void setDirecteUrl(String directeUrl) {
+        this.directeUrl = directeUrl == null ? null : directeUrl.trim();
+    }
+
+    public String getIsInvalid() {
+        return isInvalid;
+    }
+
+    public void setIsInvalid(String isInvalid) {
+        this.isInvalid = isInvalid == null ? null : isInvalid.trim();
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser == null ? null : createUser.trim();
+    }
+}
