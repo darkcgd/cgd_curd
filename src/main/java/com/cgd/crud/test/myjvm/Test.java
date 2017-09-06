e585611758257882fb3978d317667679259f9e1a
darkcgd <492404822@qq.com>
2017-09-06 20:24:19 +0800

    新增管理后台模版

---
+++ src/main/java/com/cgd/crud/test/myjvm/Test.java
@@ -0,0 +1,11 @@
+package com.cgd.crud.test.myjvm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List list=new ArrayList();
+    }
+
+}
